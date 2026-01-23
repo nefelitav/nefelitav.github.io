1142a5fa582c7b94753df19617947b07eaf986c9
nefelitav <nefeli.tavoulari123@gmail.com>
2026-01-23 21:32:30 +0100

    multiple improvements

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
 
-    // Hide all tabcontent
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    // Remove the "active" class from all tablinks
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    // Show the current tab and add the "active" class to the clicked button
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
